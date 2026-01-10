0a137d6214045d88786e984a609c00272c73a81b
Aryan Kharbanda <aryankharbanda88@gmail.com>
2026-01-10 19:05:59 +0530

    completed alpha blog REST functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,46 @@
 class ArticlesController < ApplicationController
-    def show
-      @article = Article.find(params[:id])
+  before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+
+  def show
+  end
+  def index
+    @articles = Article.all
+  end
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was creates successfully !"
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity
     end
-    def index
-        @articles = Article.all
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully!"
+      redirect_to article_path(@article)
+    else
+      render :edit, status: :unprocessable_entity
     end
-    def new
-      @article = Article.new
-    end
-    def create
-      @article = Article.new(params.require(:article).permit(:title, :description))
-      if @article.save
-        flash[:notice] = "Article was creates successfully !"
-        redirect_to article_path(@article)
-      else
-        render :new, status: :unprocessable_entity
-        flash[:notice] = "Article was creates unsuccessfully !"
-      end
-    end
+  end
+  def destroy
+   @article.destroy
+   redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
